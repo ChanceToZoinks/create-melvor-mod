5f78f65cabb98a9e2f12ee2c065c2f8d29eca1c6
ChancetoZoinks <ChanceToZoinks@users.noreply.github.com>
2023-01-21 04:36:15 -0500

    import namespace from manifest instead of changing in 2 places

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,7 +1,9 @@
+import Manifest from "../../manifest.json";
+
 export const clamp = (num: number, min: number, max: number) => {
   return Math.min(Math.max(num, min), max);
 };
 
-export const check_gamemode = (gamemode = "mod-template-gamemode") => {
-  return game.currentGamemode.namespace === gamemode;
+export const check_gamemode = () => {
+  return game.currentGamemode.namespace === Manifest.namespace;
 };
